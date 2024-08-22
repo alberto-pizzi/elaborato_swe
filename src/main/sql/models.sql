86259217c1d7b49d210763cb3477bf8da13a4937
Tommycik <tommaso.ciccotti@stud.unifi.it>
2024-08-22 12:01:10 +0200

    database updated

--- src/main/sql/models.sql
+++ src/main/sql/models.sql
@@ -42,3 +42,68 @@ CREATE TABLE IF NOT EXISTS "Facility" (
     id_owner INTEGER UNIQUE,
     FOREIGN KEY (id_owner) REFERENCES Owner(id)
 );
+
+CREATE TABLE IF NOT EXISTS "Reservation" (
+    id SERIAL PRIMARY KEY,
+    res_date VARCHAR(255) NOT NULL,
+    event_date VARCHAR(255) NOT NULL,
+    res_time VARCHAR(255) NOT NULL,
+    event_time_start VARCHAR(255) NOT NULL,
+    event_time_end VARCHAR(255) NOT NULL;
+    id_field INTEGER NOT NULL,
+    n_partecipants INT NOT NULL,
+    is_confirmed BIT NOT NULL
+    is_matched BIT NOT NULL
+    id_user INTEGER NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES User(id)
+    FOREIGN KEY (id_field) REFERENCES Field(id)
+);
+
+CREATE TABLE IF NOT EXISTS "Invite" (
+    id SERIAL PRIMARY KEY,
+    id_group INTEGER NOT NULL UNIQUE,
+    id_user INTEGER NOT NULL,
+    FOREIGN KEY (id_group) REFERENCES Group(id)
+    FOREIGN KEY (id_user) REFERENCES User(id)
+);
+
+CREATE TABLE IF NOT EXISTS "IsPart" (
+    id_group INTEGER NOT NULL UNIQUE,
+    id_user INTEGER NOT NULL,
+    PRIMARY KEY(id_group, id_user)
+    FOREIGN KEY (id_group) REFERENCES Group(id)
+    FOREIGN KEY (id_user) REFERENCES User(id)
+);
+
+CREATE TABLE IF NOT EXISTS "Group" (
+    id SERIAL PRIMARY KEY,
+    group_head INTEGER NOT NULL,
+    id_reservation INTEGER NOT NULL UNIQUE,
+    FOREIGN KEY (id_reservation) REFERENCES Reservation(id)
+    FOREIGN KEY (group_head) REFERENCES User(id)
+);
+
+CREATE TABLE IF NOT EXISTS "Field" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    id_sport INTEGER NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    price Float(3) NOT NULL,
+    image VARCHAR(100) NOT NULL,
+    id_facility INTEGER UNIQUE,
+    FOREIGN KEY (id_facility) REFERENCES Facility(id)
+);
+
+CREATE TABLE IF NOT EXISTS "Sport" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    players_required INTEGER NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS "Manages" (
+    id_facility INTEGER NOT NULL,
+    id_user INTEGER NOT NULL,
+    PRIMARY KEY(id_facility, id_user)
+    FOREIGN KEY (id_facility) REFERENCES Facility(id)
+    FOREIGN KEY (id_user) REFERENCES User(id)
+);
