a11343e44a0b77d31bd4d59e09006786a0b2cf56
alberto-pizzi <alberto.pizzi2002@outlook.com>
2024-08-23 20:22:24 +0200

    Implemented ConnectionManager

--- src/main/java/ORM/ConnectionManager.java
+++ src/main/java/ORM/ConnectionManager.java
@@ -1,5 +1,38 @@
 package main.java.ORM;
 
-public class ConnectionManager {
+import java.sql.*;
 
+public class ConnectionManager {
+    //TODO insert DB data (url, username and password)
+    private static final String url = "";
+    private static final String username = "";
+    private static final String password = "";
+    private static Connection connection = null;
+
+    // singleton instance
+    private static ConnectionManager instance = null;
+
+    private ConnectionManager(){}
+
+    public static ConnectionManager getInstance() {
+
+        if (instance == null) { instance = new ConnectionManager(); }
+
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+
+        Class.forName("org.postgresql.Driver");
+
+        if (connection == null)
+            try {
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (SQLException e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+
+        return connection;
+
+    }
 }
