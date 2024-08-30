bbf5418a75f6ba9d3c91c24aeafacd21fff0bbee
alberto-pizzi <alberto.pizzi2002@outlook.com>
2024-08-30 11:00:55 +0200

    Added main window scene.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,28 @@
 
 package main.java;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
     public static void main(String[] args){
-        System.out.println("Hello");
+        System.out.println("Start application");
+        launch(args);
     }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/main/FXML/main.fxml"));
+        stage.setTitle("JavaFX APP");
+        //TODO change window size
+        //TODO check if when quit button is pressed, app will close
+        stage.setScene(new Scene(root,300,200));
+        stage.show();
+    }
+
+
 }
