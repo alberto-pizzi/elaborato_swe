7b2bf557702c28e69f6e538c9b74f9ea4ae3fcfb
Tommycik <tommaso.ciccotti@stud.unifi.it>
2024-08-30 23:59:46 +0200

    business logic

--- src/main/java/BusinessLogic/UserAccess.java
+++ src/main/java/BusinessLogic/UserAccess.java
@@ -1,13 +1,39 @@
 package main.java.BusinessLogic;
 
+import main.java.DomainModel.User;
+import main.java.ORM.UserDAO;
+
+import java.sql.SQLException;
+
 public class UserAccess implements AccessStrategy{
     @Override
-    public void login(String username, String password) {
+    public User login(String username, String password) throws SQLException {
 
+        UserDAO dao = new UserDAO();
+        User user = null;
+        try {
+            boolean verified = dao.checkPassword(username,password);
+
+            if (verified) {
+                user = dao.getUser(username);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return user;
     }
 
     @Override
-    public void register() {
+    public void register(String username, String email, String password, String city, String province, String zip, String country) throws SQLException{
 
+        UserDAO dao = new UserDAO();
+
+        try {
+            dao.addUser(username,email,password,city,province,zip,country);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
