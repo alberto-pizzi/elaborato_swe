aaec911e8db3d606bd59d71f1c5b277ac4ae6bdd
Tommycik <tommaso.ciccotti@stud.unifi.it>
2024-08-30 23:59:06 +0200

    business logic

--- src/main/java/BusinessLogic/OwnerAccess.java
+++ src/main/java/BusinessLogic/OwnerAccess.java
@@ -1,13 +1,40 @@
 package main.java.BusinessLogic;
 
+import main.java.DomainModel.Owner;
+import main.java.ORM.OwnerDAO;
+
+import java.sql.SQLException;
+
 public class OwnerAccess implements AccessStrategy{
     @Override
-    public void login(String username, String password) {
+    public Owner login(String username, String password) throws SQLException {
 
+        OwnerDAO dao = new OwnerDAO();
+        Owner owner = null;
+        try {
+            boolean verified = dao.checkPassword(username,password);
+
+            if (verified) {
+                owner = dao.getOwner(username);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return owner;
     }
 
     @Override
-    public void register() {
+    public void register(String username, String email, String password, String city, String province, String zip, String country) throws SQLException{
 
+        OwnerDAO dao = new OwnerDAO();
+
+        try {
+            dao.addOwner(username,email,password,city,province,zip,country);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
