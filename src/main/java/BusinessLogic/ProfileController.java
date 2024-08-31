e40c77ac220ec04c68f302d11e6196471f0b8b2c
alberto-pizzi <alberto.pizzi2002@outlook.com>
2024-09-01 00:38:23 +0200

    Added parameter in ProfileController (BL)

--- src/main/java/BusinessLogic/ProfileController.java
+++ src/main/java/BusinessLogic/ProfileController.java
@@ -3,35 +3,35 @@ package main.java.BusinessLogic;
 public abstract class ProfileController {
 
     //methods
-    public void updateUsername(String newUsername){
+    public void updateUsername(String username, String newUsername){
 
     }
 
-    public void updatePassword(String newPassword){
+    public void updatePassword(String username, String newPassword){
 
     }
 
-    public void updateEmail(String newEmail){
+    public void updateEmail(String username, String newEmail){
 
     }
 
-    public void deleteProfile(){
+    public void deleteProfile(String username){
 
     }
 
-    public void updateCity(String newCity){
+    public void updateCity(String username, String newCity){
 
     }
 
-    public void updateProvince(String newProvince){
+    public void updateProvince(String username, String newProvince){
 
     }
 
-    public void updateZip(String newZip){
+    public void updateZip(String username, String newZip){
 
     }
 
-    public void updateCountry(String newCountry){
+    public void updateCountry(String username, String newCountry){
 
     }
 
