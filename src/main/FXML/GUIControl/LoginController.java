00a2484675a54d28c76b743f3060a284f46a8973
Tommycik <tommaso.ciccotti@stud.unifi.it>
2024-09-03 01:00:53 +0200

    login and signup scene

---
+++ src/main/FXML/GUIControl/LoginController.java
@@ -0,0 +1,25 @@
+package main.FXML.GUIControl;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button LogIn;
+
+    @FXML
+    private Button SignUp;
+
+    @FXML
+    private CheckBox owner;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+
+}
---
+++ src/main/FXML/GUIControl/SignUpController.java
@@ -0,0 +1,42 @@
+package main.FXML.GUIControl;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+
+    @FXML
+    private TextField city;
+
+    @FXML
+    private TextField country;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private Button logIn;
+
+    @FXML
+    private Button owner;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private PasswordField passwordConfirmed;
+
+    @FXML
+    private TextField province;
+
+    @FXML
+    private Button signUp;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField zip;
+
+}
