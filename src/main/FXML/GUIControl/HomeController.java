38799ee76a0eee0afb2b85da166c1167e0794893
Tommycik <tommaso.ciccotti@stud.unifi.it>
2024-09-07 00:36:15 +0200

    session singleton

--- src/main/FXML/GUIControl/HomeController.java
+++ src/main/FXML/GUIControl/HomeController.java
@@ -1,5 +1,6 @@
 package main.FXML.GUIControl;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -29,14 +30,19 @@ public class HomeController implements Initializable {
 
     private List<Field> fields = new ArrayList<>();
 
+    int position = 1;
+
     private List<Field> getData(){
         List<Field> fields = new ArrayList<>();
         Field field;
-        for(int i=0; i<20; i++){
+        for(int i=0; i<40; i++){
 
             field = new Field();
             fields.add(field);
         }
+        field = new Field();
+        field.setName("ra");
+        fields.add(field);
         return fields;
     }
 
@@ -47,7 +53,7 @@ public class HomeController implements Initializable {
         int columns = 0;
         int rows = 0;
         try {
-            for(int i=0; i< fields.size(); i++){
+            for(int i=0; i< 9; i++){
                 FXMLLoader fmxLoader;
                 fmxLoader = new FXMLLoader();
                 fmxLoader.setLocation(getClass().getResource("/main/FXML/fieldItem.fxml"));
@@ -67,6 +73,77 @@ public class HomeController implements Initializable {
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+
+    }
+
+    @FXML
+    private void next(ActionEvent event){
+        int columns = 0;
+        int rows = 0;
+        if(fields.size()>10*position){
+
+            try {
+                for(int i=10*position; i< 10*position+9 && i< fields.size() ; i++){
+                    FXMLLoader fmxLoader;
+                    fmxLoader = new FXMLLoader();
+                    fmxLoader.setLocation(getClass().getResource("/main/FXML/fieldItem.fxml"));
+
+                    AnchorPane anchorPane = fmxLoader.load();
+
+                    FieldItemController fieldItemController = fmxLoader.getController();
+                    fieldItemController.setData(fields.get(i));
+
+                    if(columns == 1){
+                        columns = 0;
+                        rows++;
+                    }
+
+                    fieldsGrid.add(anchorPane, columns++, rows);
+                    GridPane.setMargin(anchorPane, new Insets(10));
+
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            position++;
+        }
+
+    }
+
+    @FXML
+    private void previous(ActionEvent event){
+        int columns = 0;
+        int rows = 0;
+        if(position > 1){
+
+            try {
+                int i = 10*(position);
+                if (i > fields.size()){
+                    i = fields.size()-1;
+                }
+                for(;i> i-11 && i>0; i--){
+                    FXMLLoader fmxLoader;
+                    fmxLoader = new FXMLLoader();
+                    fmxLoader.setLocation(getClass().getResource("/main/FXML/fieldItem.fxml"));
+
+                    AnchorPane anchorPane = fmxLoader.load();
+
+                    FieldItemController fieldItemController = fmxLoader.getController();
+                    fieldItemController.setData(fields.get(i));
+
+                    if(columns == 1){
+                        columns = 0;
+                        rows++;
+                    }
+                    fieldsGrid.add(anchorPane, columns++, rows);
+                    GridPane.setMargin(anchorPane, new Insets(10));
+
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            position--;
         }
 
     }
