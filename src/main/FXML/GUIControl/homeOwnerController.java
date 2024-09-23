b337f9ca014f7ab0111c2b4537c3a731ef11e617
Tommycik <tommaso.ciccotti@stud.unifi.it>
2024-09-23 11:22:28 +0200

    home per owner

--- src/main/FXML/GUIControl/homeOwnerController.java
+++ src/main/FXML/GUIControl/homeOwnerController.java
@@ -4,9 +4,28 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+// Java program to create a pie chart
+// with some specified data
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
+import javafx.scene.layout.*;
+import javafx.event.ActionEvent;
+import javafx.scene.AmbientLight;
+import javafx.scene.shape.Sphere;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.paint.Color;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.collections.FXCollections;
 
 import java.io.IOException;
 import java.net.URL;
@@ -17,10 +36,16 @@ public class homeOwnerController implements Initializable {
 
 
     @FXML
+    private BarChart<?, ?> barChart;
+
+    @FXML
     private Label dailyMoney;
 
     @FXML
     private Label monthlyMoney;
+
+    @FXML
+    private PieChart pieChart;
 
     @FXML
     private Label reservationsNumber;
@@ -29,6 +54,16 @@ public class homeOwnerController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        PieChart.Data[] data = new PieChart.Data[2];
+        int[] values = {20, 30};
+
+        String[] status = {"Reserved field", "Available field"};
+        for (int i = 0; i<data.length; i++) {
+            data[i] = new PieChart.Data(status[i], values[i]);
+        }
+
+        PieChart pie_chart = new PieChart(FXCollections.observableArrayList(data));
+
     }
 
 
--- src/main/java/BusinessLogic/OwnerProfileController.java
+++ src/main/java/BusinessLogic/OwnerProfileController.java
@@ -1,9 +1,19 @@
 package main.java.BusinessLogic;
 
 import main.java.DomainModel.Owner;
+import main.java.DomainModel.User;
+
 public class OwnerProfileController extends ProfileController {
     //attributes
     private Owner owner;
+
+    public OwnerProfileController(Owner owner) {
+        this.owner = owner;
+    }
+
+    public OwnerProfileController() {
+        this.owner = (Owner) SessionController.getInstance().getPerson();
+    }
 
     //getters
     public Owner getOwner() {
