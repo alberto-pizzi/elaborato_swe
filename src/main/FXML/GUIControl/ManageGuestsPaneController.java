66344d2413598f16fe1ee6b43252d4693726ff41
alberto-pizzi <alberto.pizzi2002@outlook.com>
2025-03-06 20:36:13 +0100

    Added new FXML, with its GUI controller, of selection guests pane

--- src/main/FXML/GUIControl/ManageGuestsPaneController.java
+++ src/main/FXML/GUIControl/ManageGuestsPaneController.java
@@ -1,4 +1,42 @@
 package main.FXML.GUIControl;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class ManageGuestsPaneController extends SelectGuestsPaneController {
+
+    @FXML
+    protected Button forceAddButton;
+
+    @FXML
+    protected Button removeAllMembersButton;
+
+    @FXML
+    protected Button removeGroupMemberButton;
+
+    @FXML
+    protected Label guestUsersWALabel;
+
+
+
+    @FXML
+    public void handleForceAddButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void handleRemoveGroupMemberButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void handleRemoveAllMembersButton(ActionEvent event) {
+
+    }
+
+
+
+
 }
