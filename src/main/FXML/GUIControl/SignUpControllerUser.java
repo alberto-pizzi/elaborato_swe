61c678cb78766cb49156a0dd3c26d1f353a1a39e
alberto-pizzi <alberto.pizzi2002@outlook.com>
2025-03-14 23:27:52 +0100

    Added fixme

--- src/main/FXML/GUIControl/SignUpControllerUser.java
+++ src/main/FXML/GUIControl/SignUpControllerUser.java
@@ -70,7 +70,7 @@ public class SignUpControllerUser implements Initializable {
         AccessController access = null;
         access = new AccessController(new UserAccess());
         System.out.println("User ");
-
+        //FIXME fix text field texts
         if(!(password == null || username == null || email == null || province == null)) {
 
             if(password.getText().equals(passwordConfirmed.getText())) {
