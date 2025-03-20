aad7e2ae6799e86f039dfd7a53464cd0272c6d1f
Tommycik <tommaso.ciccotti@stud.unifi.it>
2025-03-20 20:22:47 +0100

    ereditarietà announcement e facility choice finita

---
+++ src/main/FXML/GUIControl/AnnouncementController.java
@@ -0,0 +1,120 @@
+package main.FXML.GUIControl;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import main.java.BusinessLogic.ManagerOwnerManagementController;
+import main.java.DomainModel.Field;
+import main.java.DomainModel.Reservation;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public abstract class AnnouncementController implements Initializable {
+
+    @FXML
+    protected Button cancelButton;
+
+    @FXML
+    protected Label errorMessage;
+
+    @FXML
+    protected Label letterCounter;
+
+    @FXML
+    protected Button confirmButton;
+
+    @FXML
+    protected TextArea messageText;
+
+    protected Reservation reservation;
+
+    protected BorderPane menuPane;
+
+    MessagesController messagesController;
+
+    ManagerOwnerManagementController managerOwnerManagementController;
+
+    protected int messageLimit = 250;
+
+    protected Field reservationField;
+
+    protected abstract void changeView() throws IOException, SQLException, ClassNotFoundException;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        messageText.setEditable(true);
+        messageText.setText("Insert you text here.");
+        this.messagesController = new MessagesController(errorMessage);
+        this.managerOwnerManagementController = new ManagerOwnerManagementController();
+
+        messageText.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            public void handle(KeyEvent event) {
+                letterCounter.setText(String.valueOf(messageText.getText().length())+ "/" + messageLimit);
+                if(messageText.getText().length() >= messageLimit) {
+                    messagesController.showMessage("Too many characters in the message.The maximum is " + messageLimit, MessagesController.MessageType.ERROR,5);
+                    errorMessage.setAlignment(Pos.CENTER);
+                }
+            }
+        });
+    }
+
+
+    public void setData(BorderPane menuPane, Reservation reservation) {
+        this.menuPane = menuPane;
+        this.reservation = reservation;
+    }
+
+    @FXML
+    public void handleCancelButton(ActionEvent event) throws SQLException, ClassNotFoundException, IOException {
+        reservationField = managerOwnerManagementController.getReservationField(reservation);
+        System.out.println("Cancelled!");
+        changeView();
+    }
+
+    @FXML
+    public void handleConfirmButton(ActionEvent event) throws SQLException, ClassNotFoundException, IOException {
+
+        if(messageText.getText().length() < messageLimit && !messageText.getText().isEmpty()) {
+
+            System.out.println("Confirm button clicked: ");
+
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirm announcement");
+            alert.setHeaderText("Make announcement");
+            alert.setContentText("Are you sure you want to make this announcement?");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if(result.get() == ButtonType.OK){
+
+                reservationField = managerOwnerManagementController.getReservationField(reservation);
+                managerOwnerManagementController.reservationAnnouncement(messageText.getText(), reservation);
+                System.out.println("Sent!");
+                changeView();
+
+            } else if(result.get() == ButtonType.CANCEL){
+                System.out.println("Cancel!");
+            }
+        } else if (messageText.getText().isEmpty()) {
+            messagesController.showMessage("The message is blank", MessagesController.MessageType.ERROR,5);
+            errorMessage.setAlignment(Pos.CENTER);
+        }else {
+            messagesController.showMessage("Too many characters in the message.The maximum is " + messageLimit, MessagesController.MessageType.ERROR,5);
+            errorMessage.setAlignment(Pos.CENTER);
+        }
+
+
+    }
+}
---
+++ src/main/FXML/GUIControl/FacilityChoice.java
@@ -0,0 +1,122 @@
+package main.FXML.GUIControl;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import main.java.DomainModel.Facility;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public abstract class FacilityChoice implements Initializable {
+
+    @FXML
+    protected VBox facilityList;
+
+    @FXML
+    protected Button next;
+
+    @FXML
+    protected Label pageNumber;
+
+    @FXML
+    protected Button previous;
+
+    protected AnchorPane page;
+
+    protected List<Facility> facilities = new ArrayList<>();
+
+    protected int currentPage = 1;
+
+    protected int itemsPerPage = 3;
+
+    protected BorderPane menuPane;
+
+    public BorderPane getMenuPane() {
+        return menuPane;
+    }
+
+    public AnchorPane getPage() {
+        return page;
+    }
+
+    abstract protected List<Facility> getData() throws SQLException, ClassNotFoundException;
+
+    abstract protected void setFacilities(int i) throws IOException, SQLException;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        try {
+            facilities.addAll(getData());
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String page = String.valueOf(currentPage);
+        pageNumber.setText(page);
+    }
+
+    public void setData(BorderPane menuPane) {
+        this.menuPane = menuPane;
+        for(int i=0; i < itemsPerPage && i < facilities.size(); i++){
+            try {
+                setFacilities(i);
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @FXML
+    public void handleNextButton(ActionEvent event){
+
+        if(facilities.size()>itemsPerPage* currentPage) {
+            facilityList.getChildren().clear();
+
+            for (int i = itemsPerPage * currentPage; i < itemsPerPage * (currentPage+1)  && i < facilities.size(); i++) {
+                try {
+                    setFacilities(i);
+                } catch (IOException | SQLException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
+            currentPage++;
+            pageNumber.setText(String.valueOf(currentPage));
+        }
+
+    }
+
+    @FXML
+    public void handlePreviousButton(ActionEvent event){
+
+        if(currentPage > 1){
+            facilityList.getChildren().clear();
+
+            for(int i = itemsPerPage*(currentPage -1)-1; i > itemsPerPage*(currentPage -2)-1 && i>=0; i--){
+                try {
+                    setFacilities(i);
+                } catch (IOException | SQLException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
+            currentPage--;
+            pageNumber.setText(String.valueOf(currentPage));
+        }
+    }
+}
