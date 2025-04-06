0607daffbde41ee9fce9c534a31dc0b48becad7c
alberto-pizzi <alberto.pizzi2002@outlook.com>
2025-04-06 15:47:59 +0200

    Added tests into UserDAOTest

--- test/tests/ORMTest/PersonDAOTest.java
+++ test/tests/ORMTest/PersonDAOTest.java
@@ -1,10 +1,15 @@
 package tests.ORMTest;
 
+import main.java.DomainModel.Person;
 import main.java.ORM.PersonDAO;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-
+import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 
 public abstract class PersonDAOTest extends GeneralDAOTest {
 
--- test/tests/ORMTest/UserDAOTest.java
+++ test/tests/ORMTest/UserDAOTest.java
@@ -3,12 +3,12 @@ package tests.ORMTest;
 import main.java.DomainModel.User;
 import main.java.ORM.UserDAO;
 import org.junit.Before;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assumptions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 
 import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class UserDAOTest extends PersonDAOTest{
 
@@ -26,27 +26,8 @@ public class UserDAOTest extends PersonDAOTest{
             shouldSkip = true;
 
 
-        Assumptions.assumeTrue(shouldSkip);
+        Assumptions.assumeFalse(shouldSkip);
 
-    }
-
-    @Override
-    protected User createUser() throws SQLException {
-        //pay attention to userId
-        User user = super.createUser(); //TODO is super good? Or new object is better?
-
-        if (personDAO != null){
-            int idUser = personDAO.addUser(user.getUsername(), user.getEmail(), user.getPassword(), user.getCity(), user.getProvince(), user.getZip(), user.getCountry());
-
-            if (idUser != 0)
-                user.setId(idUser);
-            else
-                return null;
-        }
-        else
-            return null;
-
-        return user;
     }
 
     @Override
@@ -62,6 +43,166 @@ public class UserDAOTest extends PersonDAOTest{
         user = null;
         personDAO = null;
     }
+
+    @Override
+    protected User createUser() throws SQLException {
+        //pay attention to userId
+        User user = new User(0,"hello@gmail.com","user1","hello123","London","London","00000","UK");
+
+        if (personDAO != null){
+            int idUser = personDAO.addUser(user.getUsername(), user.getEmail(), user.getPassword(), user.getCity(), user.getProvince(), user.getZip(), user.getCountry());
+
+            if (idUser != 0)
+                user.setId(idUser);
+            else
+                return null;
+        }
+        else
+            return null;
+
+        return user;
+    }
+
+
+
+    //TODO is it correct?
+    @Test
+    public void getUserTest() throws SQLException {
+        User user = personDAO.getUser(this.user.getUsername());
+
+        if (user == null)
+            shouldSkip = true;
+
+        assertNotNull(user);
+
+    }
+
+    @Test
+    public void updateEmailTest() throws SQLException {
+
+
+        String newEmail = "newmail@gmail.com";
+
+        personDAO.updateEmail(user.getUsername(), newEmail);
+        User userUpdated = personDAO.getUser(user.getUsername());
+
+        assertEquals(userUpdated.getEmail(), newEmail);
+
+    }
+
+    @Test
+    public void updateUsernameTest() throws SQLException {
+
+        String newUsername = "newUser";
+
+        personDAO.updateUsername(user.getUsername(), newUsername);
+        user.setUsername(newUsername);
+        User userUpdated = personDAO.getUser(newUsername);
+
+        assertNotNull(userUpdated);
+
+        assertEquals(userUpdated.getUsername(), newUsername);
+
+    }
+
+    @Test
+    public void updateProvinceTest() throws SQLException {
+
+        String newProvince = "newProvince";
+
+        personDAO.updateProvince(user.getUsername(), newProvince);
+        User userUpdated = personDAO.getUser(user.getUsername());
+
+        assertEquals(userUpdated.getProvince(), newProvince);
+
+    }
+
+    @Test
+    public void updateCountryTest() throws SQLException {
+
+        String newCountry = "newCountry";
+
+        personDAO.updateCountry(user.getUsername(), newCountry);
+        User userUpdated = personDAO.getUser(user.getUsername());
+
+        assertEquals(userUpdated.getCountry(), newCountry);
+
+    }
+
+    @Test
+    public void updateCityTest() throws SQLException {
+
+        String newCity = "newCity";
+
+        personDAO.updateCity(user.getUsername(), newCity);
+        User userUpdated = personDAO.getUser(user.getUsername());
+
+        assertEquals(userUpdated.getCity(), newCity);
+
+    }
+
+    @Test
+    public void updateZipTest() throws SQLException {
+
+        String newZip = "12345";
+
+        personDAO.updateZip(user.getUsername(), newZip);
+        User userUpdated = personDAO.getUser(user.getUsername());
+
+        assertEquals(userUpdated.getZip(), newZip);
+
+    }
+
+    @Test
+    public void updatePasswordTest() throws SQLException {
+
+        //TODO is cryptography needed?
+        String newPassword = "newPassword";
+
+        personDAO.updatePassword(user.getUsername(), newPassword);
+        User userUpdated = personDAO.getUser(user.getUsername());
+
+        assertEquals(userUpdated.getPassword(), newPassword);
+
+    }
+
+
+    @Test
+    public void checkEmailExistenceTest() throws SQLException {
+
+        assertTrue(personDAO.checkEmailExistence(user.getEmail()));
+
+        String fakeEmail = "fakeemail@gmail.com";
+
+        assertFalse(personDAO.checkEmailExistence(fakeEmail));
+
+    }
+
+    @Test
+    public void getUsersByProvinceTest() throws SQLException, ClassNotFoundException {
+        assertFalse(personDAO.getUsersByProvince(user.getProvince()).isEmpty());
+    }
+
+    @Test
+    public void getUserByIDTest() throws SQLException, ClassNotFoundException {
+        assertNotNull(personDAO.getUserByID(user.getId()));
+
+        assertNull(personDAO.getUserByID(user.getId()+20));
+    }
+
+
+    @Test
+    public void getEncodedPasswordTest() throws SQLException, ClassNotFoundException {
+        assertFalse(personDAO.getEncodedPassword(user.getUsername()).isEmpty());
+    }
+
+    @Test
+    public void addUserTest() throws SQLException {
+        //TODO should be implement?
+    }
+
+
+
 
 
 
