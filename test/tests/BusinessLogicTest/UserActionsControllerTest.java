99740696b8c7ed4637763ae355ceb1831fc54276
alberto-pizzi <alberto.pizzi2002@outlook.com>
2025-04-10 23:52:21 +0200

    Refactoring

---
+++ test/tests/BusinessLogicTest/UserActionsControllerTest.java
@@ -0,0 +1,26 @@
+package tests.BusinessLogicTest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.sql.SQLException;
+
+
+public class UserActionsControllerTest extends GeneralBSTest {
+
+    @Override
+    @BeforeEach
+    public void setup() throws SQLException {
+
+
+
+
+    }
+
+    @Override
+    @AfterEach
+    public void teardown(){
+
+
+    }
+}
