1fbd36793a33426af80bf33f93fd002d46d86370
alberto-pizzi <alberto.pizzi2002@outlook.com>
2025-04-11 00:24:27 +0200

    Start to implement BS test

--- test/tests/BusinessLogicTest/UserActionsControllerTest.java
+++ test/tests/BusinessLogicTest/UserActionsControllerTest.java
@@ -1,16 +1,53 @@
 package tests.BusinessLogicTest;
 
+import main.java.BusinessLogic.UserActionsController;
+import main.java.DomainModel.User;
+import main.java.ORM.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.sql.SQLException;
 
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class UserActionsControllerTest extends GeneralBSTest {
+
+    private UserActionsController userActionsController;
+
+    private User user = null;
 
     @Override
     @BeforeEach
     public void setup() throws SQLException {
+
+        user = createUser();
+
+        //TODO implement
+        /*
+
+        UserDAO userDAOMock = mock(UserDAO.class);
+
+
+        GroupDao groupDaoMock = mock(GroupDao.class);
+
+        IsPartDao isPartDaoMock = mock(IsPartDao.class);
+
+        WorkingHoursDAO workingHoursDAOMock = mock(WorkingHoursDAO.class);
+
+        ReservationDao reservationDaoMock = mock(ReservationDao.class);
+
+        InviteDao inviteDaoMock = mock(InviteDao.class);
+
+        FieldDao fieldDaoMock = mock(FieldDao.class);
+
+        ManagesDAO managesDAOMock = mock(ManagesDAO.class);
+
+
+         */
+
+
 
 
 
@@ -20,7 +57,6 @@ public class UserActionsControllerTest extends GeneralBSTest {
     @Override
     @AfterEach
     public void teardown(){
-
 
     }
 }
