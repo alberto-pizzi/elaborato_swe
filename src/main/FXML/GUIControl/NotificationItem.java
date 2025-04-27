810ec69b232a8763f3187e4cf1c54f4bb3b7f163
Tommycik <tommaso.ciccotti@stud.unifi.it>
2025-04-27 18:37:53 +0200

    fatti alcuni allacci e messaggi di errore e fatta eridatarietà notification

---
+++ src/main/FXML/GUIControl/NotificationItem.java
@@ -0,0 +1,72 @@
+package main.FXML.GUIControl;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import main.java.BusinessLogic.NotificationController;
+import main.java.DomainModel.Notification;
+
+import java.sql.SQLException;
+
+public class NotificationItem {
+
+    @FXML
+    protected Button deleteNotificationButton;
+
+    @FXML
+    protected Label notificationMessageLabel;
+
+    @FXML
+    protected Button gotoButton;
+
+    @FXML
+    protected Label notificationTitleLabel;
+
+    @FXML
+    protected AnchorPane notificationItemPane;
+
+    protected Notification notification;
+
+    protected Notifications notifications;
+
+    //todo testare
+    public void setNotificationsController(Notifications notificationsController) {
+        this.notifications = notificationsController;
+    }
+
+    public void setData(Notification tmpNotification) {
+        this.notification = tmpNotification;
+
+        //FIXME better position for message builder?
+        notification.buildMessage();
+
+        notificationTitleLabel.setText(notification.getTitle());
+        notificationMessageLabel.setText(notification.getMessage());
+    }
+
+    @FXML
+    public void handleGotoButton(ActionEvent event) {
+
+        //TODO implement
+    }
+
+    @FXML
+    public void handleDeleteNotificationButton(ActionEvent event) throws SQLException {
+
+        System.out.println("Deleting button clicked: " + notificationTitleLabel.getText());
+        System.out.println("Deleting button clicked ID: " + notification.getId());
+
+        NotificationController notificationController = new NotificationController();
+        if(notificationController.deleteNotifications(notification)){
+            if (notifications != null) {
+                notifications.removeNotificationItemFromGUI(notificationItemPane,notification);
+            }
+        }else{
+            //todo aggiungere messaggio di errore
+        }
+
+
+    }
+}
--- src/main/FXML/GUIControl/NotificationItemController.java
+++ src/main/FXML/GUIControl/NotificationItemController.java
@@ -11,66 +11,6 @@ import main.java.DomainModel.Notification;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 
-public class NotificationItemController {
-
-    @FXML
-    private Button deleteNotificationButton;
-
-    @FXML
-    private Label notificationMessageLabel;
-
-    @FXML
-    private Button gotoButton;
-
-    @FXML
-    private Label notificationTitleLabel;
-
-    @FXML
-    private AnchorPane notificationItemPane;
-
-    private Notification notification;
-
-    private NotificationsController notificationsController;
-
-    public void setNotificationsController(NotificationsController notificationsController) {
-        this.notificationsController = notificationsController;
-    }
-
-    public void setData(Notification tmpNotification) {
-        this.notification = tmpNotification;
-
-        //FIXME better position for message builder?
-        notification.buildMessage();
-
-        notificationTitleLabel.setText(notification.getTitle());
-        notificationMessageLabel.setText(notification.getMessage());
-
-    }
-
-    @FXML
-    void handleDeleteNotificationButton(ActionEvent event) throws SQLException {
-
-        System.out.println("Deleting button clicked: " + notificationTitleLabel.getText());
-        System.out.println("Deleting button clicked ID: " + notification.getId());
-
-
-        NotificationController notificationController = new NotificationController();
-        if(notificationController.deleteNotifications(notification)){
-            if (notificationsController != null) {
-                notificationsController.removeNotificationItemFromGUI(notificationItemPane,notification);
-            }
-        }else{
-            //todo aggiungere messaggio di errore
-        }
-
-
-    }
-
-    @FXML
-    void handleGotoButton(ActionEvent event) {
-
-        //TODO implement
-    }
-
+public class NotificationItemController extends NotificationItem{
 
 }
