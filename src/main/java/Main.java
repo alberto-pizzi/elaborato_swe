4ffc70d2292371251441b0b01bd098321887f739
Tommycik <107619877+Tommycik@users.noreply.github.com>
2025-05-06 19:14:32 +0200

    gui finita tommaso

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
             stage.show();
             stage.setResizable(false);
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e);
         }
     }
 
