97e3b9f7a21eb423de837187659ba85b1f2463ce
alberto-pizzi <alberto.pizzi2002@outlook.com>
2025-05-08 10:34:25 +0200

    Updated ReservationTest

--- test/tests/DomainModelTest/ReservationTest.java
+++ test/tests/DomainModelTest/ReservationTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.sql.Date;
 import java.sql.Time;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,6 +26,39 @@ public class ReservationTest extends GeneralTest{
         assertTrue(reservationMatched.isMatched());
         assertFalse(reservationMatched.isConfirmed());
         assertFalse(reservationMatched.isDeleted());
+
+    }
+
+    @Test
+    public void isEndTimeAfterThanStartTimeTest(){
+
+        LocalTime startTime = LocalTime.of(23, 0);
+        LocalTime endTime = startTime.plusHours(1);
+        LocalDate eventDate = LocalDate.of(2020, 1, 1);
+
+        assertFalse(Reservation.isEndTimeAfterThanStartTime(startTime, endTime, eventDate));
+
+        startTime = LocalTime.of(14, 0);
+        endTime = startTime.plusHours(1);
+
+        assertTrue(Reservation.isEndTimeAfterThanStartTime(startTime, endTime, eventDate));
+
+    }
+
+    @Test
+    public void isTimeOverlappingTest(){
+
+        LocalTime startTime = LocalTime.of(14, 0);
+        LocalTime endTime = LocalTime.of(16, 0);
+
+        LocalTime startTime2 = LocalTime.of(17, 0);
+        LocalTime endTime2 = LocalTime.of(18, 0);
+
+        assertFalse(Reservation.isTimeOverlapping(startTime, endTime, startTime2, endTime2));
+
+        startTime2 = LocalTime.of(15, 0);
+
+        assertTrue(Reservation.isTimeOverlapping(startTime, endTime, startTime2, endTime2));
 
     }
 
